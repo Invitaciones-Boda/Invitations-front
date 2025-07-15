295199dd504fe13295c80bf347cf7068cf0bc3a0
Kevin Muñoz <125827081+KevinJJ01@users.noreply.github.com>
2025-07-14 23:33:07 -0500

    Initial commit- dev of view initial
    Se realiza la vista principal, falta arreglar diseño y agregar función de validación de código Back end.

---
+++ home/home.js
@@ -0,0 +1,50 @@
+
+
+
+// FUNCION PARA INGRESAR A LA INVITACION
+window.ingresarInvitaion = async function (){
+    let valor = document.getElementById("code").value;
+    if(!valor){
+        alert('No se recibio un codigo de invitación');
+    }
+    alert(valor)
+}
+
+// FUNCION PARA ABRIR LA TARJETA DE INVITACION
+window.openTargetInvitation = function() {
+    // Tomar los elementos
+    const targetEl = document.getElementById("target_invitation");
+    const triangleEl = document.getElementById("triangle-invitation");
+    //Validar si existen las cards 
+    if (!targetEl || !triangleEl) {
+        console.error("No se encontraron los elementos.");
+        return;
+    }
+    // Elimincar clases anteriores
+    targetEl.classList.remove("target_invitation");
+    triangleEl.classList.remove("triangle-invitation");
+    // Agregar clases que puedes definir en CSS
+    targetEl.classList.add("target_invitation_closed");
+    triangleEl.classList.add("triangle-invitation-closed");
+
+    setTimeout(() => {
+        openPopUp();
+    }, 500);
+    
+};
+
+
+// FUNCION PARA ABRIR EL POPUP DE LA INVITACION 
+window.openPopUp = function () {
+  // Tomar el elemento
+  const popUp = document.getElementById("popup-invitation");
+
+  if (!popUp) {
+    console.error("No se encontró el elemento con id 'popup-invitation'");
+    return;
+  }
+
+  // Cambiar display a flex
+  popUp.style.display = "flex";
+};
+
