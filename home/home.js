42d3952849ec825227303379187807e5c6d3d1a6
Kevin Muñoz <125827081+KevinJJ01@users.noreply.github.com>
2025-07-23 21:41:17 -0500

    implementado diseño parte 1

--- home/home.js
+++ home/home.js
@@ -1,86 +1,80 @@
 // FUNCION PARA INGRESAR AL BACKEND USANDO AJAX
 async function ingresar(valor) {
-    return new Promise(function (resolve, reject) {
-        $.ajax({
-            url: 'http://localhost:8000/invitation/ingreso/',
-            type: 'POST',
-            data: { codigo: valor },
-            success: function (response) {
-                resolve(response);
-            },
-            error: function (error) {
-                reject(error);
-            }
-        });
+  return new Promise(function (resolve, reject) {
+    $.ajax({
+      url: "http://localhost:8000/invitation/ingreso/",
+      type: "POST",
+      data: { codigo: valor },
+      success: function (response) {
+        resolve(response);
+      },
+      error: function (error) {
+        reject(error);
+      },
     });
+  });
 }
 
 // FUNCION PARA INGRESAR A LA INVITACION
 async function ingresarInvitaion() {
-    let valor = document.getElementById("code").value;
-    if (!valor) {
-        alert('No se recibió un código de invitación');
-        return;
+  let valor = document.getElementById("code").value;
+  if (!valor) {
+    alert("No se recibió un código de invitación");
+    return;
+  }
+
+  try {
+    let state = false;
+
+    if (valor === "ABC123") {
+      state = true;
+    } else {
+      state = await ingresar(valor);
     }
 
-    try {
-
-        let state = false;
-
-        if (valor === "ABC123"){
-            state = true
-        }else{
-            state = await ingresar(valor);
-        }
-
-        if (state) {
-            location.href = "/invitation/invitation.html";
-        }
-
-    } catch (error) {
-        // Manejo seguro del mensaje de error
-        let mensaje = error?.responseJSON.message || "Error al procesar la solicitud.";
-        alert(mensaje);
+    if (state) {
+      location.href = "/invitation/invitation.html";
     }
-
+  } catch (error) {
+    // Manejo seguro del mensaje de error
+    let mensaje =
+      error?.responseJSON.message || "Error al procesar la solicitud.";
+    alert(mensaje);
+  }
 }
-
 
 // FUNCION PARA ABRIR LA TARJETA DE INVITACION
 window.openTargetInvitation = function () {
-    // Tomar los elementos
-    const targetEl = document.getElementById("target_invitation");
-    const triangleEl = document.getElementById("triangle-invitation");
-    //Validar si existen las cards 
-    if (!targetEl || !triangleEl) {
-        console.error("No se encontraron los elementos.");
-        return;
-    }
-    // Elimincar clases anteriores
-    targetEl.classList.remove("target_invitation");
-    triangleEl.classList.remove("triangle-invitation");
-    // Agregar clases que puedes definir en CSS
-    targetEl.classList.add("target_invitation_closed");
-    triangleEl.classList.add("triangle-invitation-closed");
+  // Tomar los elementos
+  const targetEl = document.getElementById("target_invitation");
+  const triangleEl = document.getElementById("triangle-invitation");
+  //Validar si existen las cards
+  if (!targetEl || !triangleEl) {
+    console.error("No se encontraron los elementos.");
+    return;
+  }
+  // Elimincar clases anteriores
+  targetEl.classList.remove("target_invitation");
+  triangleEl.classList.remove("triangle-invitation");
+  // Agregar clases que puedes definir en CSS
+  targetEl.classList.add("target_invitation_closed");
+  triangleEl.classList.add("triangle-invitation-closed");
 
-    setTimeout(() => {
-        openPopUp();
-    }, 500);
-
+  setTimeout(() => {
+    openPopUp();
+  }, 500);
 };
 
-
-// FUNCION PARA ABRIR EL POPUP DE LA INVITACION 
+// FUNCION PARA ABRIR EL POPUP DE LA INVITACION
 window.openPopUp = function () {
-    // Tomar el elemento
-    const popUp = document.getElementById("popup-invitation");
+  // Tomar el elemento
+  const popUp = document.getElementById("popup-invitation");
 
-    if (!popUp) {
-        console.error("No se encontró el elemento con id 'popup-invitation'");
-        return;
-    }
+  if (!popUp) {
+    console.error("No se encontró el elemento con id 'popup-invitation'");
+    return;
+  }
 
-    // Cambiar display a flex
-    popUp.style.display = "flex";
+  // Cambiar display a flex
+  popUp.style.display = "flex";
 };
-
